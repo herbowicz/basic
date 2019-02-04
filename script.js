91ec6cf462e2277a1e2828473b5ab0a123d086f7
Greg Herbowicz <herbowicz@gmail.com>
2019-02-04 22:19:30 +0100

    winyl1

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert('ok');
+// var cover = document.querySelector('.cover');
+
+// cover.addEventListener('click', slide);
+// function slide() {
+//     console.log(this);
+//     this.children[1].style.left = '250px';
+// }
