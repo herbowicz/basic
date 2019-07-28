88ed2128ba988301748899425a743d3631bef164
Greg Herbowicz <herbowicz@gmail.com>
2019-07-28 09:00:13 +0200

    basic webpack hot reload

--- script.js
+++
@@ -1,7 +0,0 @@
-// var cover = document.querySelector('.cover');
-
-// cover.addEventListener('click', slide);
-// function slide() {
-//     console.log(this);
-//     this.children[1].style.left = '250px';
-// }
